f13df1e1a15023ed38110944a352370e87b95c98
Rajesh R <rajesh.r11@kyndryl.com>
2025-03-28 22:10:13 +0530

    added datavalidation code

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -10,14 +10,14 @@ import os
 import sys
 import numpy as np
 import pandas as pd
-import pymongo
 from typing import List
 from sklearn.model_selection import train_test_split
 from dotenv import load_dotenv
 load_dotenv()
+from pymongo.mongo_client import MongoClient
 
 MONGO_DB_URL=os.getenv("MONGO_DB_URL")
-
+print(MONGO_DB_URL)
 
 class DataIngestion:
     def __init__(self,data_ingestion_config:DataIngestionConfig):
@@ -33,7 +33,7 @@ class DataIngestion:
         try:
             database_name=self.data_ingestion_config.database_name
             collection_name=self.data_ingestion_config.collection_name
-            self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
+            self.mongo_client=MongoClient(MONGO_DB_URL)
             collection=self.mongo_client[database_name][collection_name]
 
             df=pd.DataFrame(list(collection.find()))
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -7,3 +7,12 @@ class DataIngestionArtifact:
     #feature_store_file_path: str
     #is_ingested: bool = False
     #message: str = None
+
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
